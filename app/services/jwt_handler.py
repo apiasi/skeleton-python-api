e35f05cf1bdde520a811122e7f4318c73030d205
Artur Piasi <artur.piasi@gmail.com>
2025-03-08 17:31:49 -0300

    Atualiza a criação de tokens de acesso para usar datetime.now em vez de datetime.utcnow

--- app/services/jwt_handler.py
+++ app/services/jwt_handler.py
@@ -9,9 +9,9 @@ def create_access_token(
 ):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow(timezone.utc) + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
